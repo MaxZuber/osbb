63c9c472655aec62b0e3dbfc960d4d44afcf6242
= <max@brainyyack.com>
2016-03-23 22:28:29 +0200

    test

--- WebProject/WebApplication/XCL/XCL.Core/Services/Impl/SecurityService.cs
+++ WebProject/WebApplication/XCL/XCL.Core/Services/Impl/SecurityService.cs
@@ -31,6 +31,7 @@ namespace XCL.Core.Services.Impl
 
         public void AuthenticateRequest()
         {
+
             if (HttpContext.Current.User != null && HttpContext.Current.User.Identity != null && HttpContext.Current.User.Identity.IsAuthenticated == true)
             {
                 IIdentity identity = HttpContext.Current.User.Identity;
